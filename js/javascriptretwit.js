f9d7382afbb39a6ddb6f677d7f2ee384cb0562db
neoxyos <super_noki@live.fr>
2015-04-03 12:13:52 +0200

    ajout twit fonctionnel

--- js/javascriptretwit.js
+++ js/javascriptretwit.js
@@ -1,5 +1,26 @@
 $("#container").children("form").attr("action", "#");
 
+// Twit
+function twit(event) {
+    event.preventDefault(); // Empêcher le rechargement de la page.
+    var idMessage = $(this).attr("data-id");
+    var idUser = $(this).attr("data-user");
+    var action = "twit";
+    $.ajax({
+        type: 'POST', 
+        url: "index.php",
+        data: {
+            id: idMessage,
+            origin: idUser,
+            action: action}, 
+        success: function (r) {
+            $("body").html(r);           
+        }
+    });
+}
+$(".RT").on("click", twit);
+
+
 // Retwit
 function retwit(event) {
     event.preventDefault(); // Empêcher le rechargement de la page.
